a7d1ac214e637fcd91e1bf5f108f9ff51b849716
zhoaib <zhoaibhassan@gmail.com>
2022-10-19 22:45:24 +0600

    edit data

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('/data/news.json')
+
 
 app.get('/', (req, res) => {
     res.send('News API');
@@ -14,6 +16,10 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/news/id:', (req, res) => {
+    console.log(req.params);
+})
 
 app.listen(port, () => {
     console.log('Dragon News Server Running on port-', port)
